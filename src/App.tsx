0822a2182b03f108484927343411638aeaea84cc
Brent Van Hese <brent.vanhese@icapps.com>
2025-07-01 23:47:37 +0200

    basic setup of i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useTranslation } from "react-i18next";
 import { ModeToggle } from "./_components/ModeToggle";
+import { Button } from "./components/ui/button";
+import { Language } from "./_translations/i18n";
 
 function App() {
+  const { i18n, t } = useTranslation();
+
+  console.log("Current language:", i18n.language);
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="text-center">
@@ -9,6 +16,19 @@ function App() {
           The verry beginning of the convention website of family Van Hese
         </p>
         <ModeToggle />
+        <Button
+          onClick={() => i18n.changeLanguage(Language.en)}
+          disabled={i18n.language === Language.en}
+        >
+          {"CHANGE LANG TO EN"}
+        </Button>
+        <Button
+          onClick={() => i18n.changeLanguage(Language.nl)}
+          disabled={i18n.language === Language.nl}
+        >
+          {"CHANGE LANG TO NL"}
+        </Button>
+        <p>{t("HOME.TITLE")}</p>
       </div>
     </div>
   );
