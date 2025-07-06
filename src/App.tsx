d31a5f3b88f912c6c7b2ec3402b7581475338425
Brent Van Hese <brent.vanhese@icapps.com>
2025-07-07 00:03:50 +0200

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useTranslation } from "react-i18next";
-import { ModeToggle } from "./_components/ModeToggle";
-import { Button } from "./components/ui/button";
-import { Language } from "./_translations/i18n";
 import { useEffect } from "react";
+import { Navigate, Outlet, Route, Routes } from "react-router";
+import Home from "./home/Home";
 
 function App() {
-  const { i18n, t } = useTranslation("HOME");
+  const { i18n } = useTranslation();
 
   useEffect(() => {
     const previousLanguage = localStorage.getItem("i18nextLng");
@@ -16,34 +15,22 @@ function App() {
     }
   }, [i18n]);
 
-  const handleLanguageChange = (language: string) => {
-    i18n.changeLanguage(language);
-    localStorage.setItem("i18nextLng", language);
-  };
-
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div className="text-center">
-        <p className="text-3xl font-bold underline">WORK IN PROGRESS</p>
-        <p className="text-xl italic mt-4">
-          The verry beginning of the convention website of family Van Hese
-        </p>
-        <ModeToggle />
-        <Button
-          onClick={() => handleLanguageChange(Language.en)}
-          disabled={i18n.language === Language.en}
-        >
-          {"CHANGE LANG TO EN"}
-        </Button>
-        <Button
-          onClick={() => handleLanguageChange(Language.nl)}
-          disabled={i18n.language === Language.nl}
-        >
-          {"CHANGE LANG TO NL"}
-        </Button>
-        <p>{t("TITLE")}</p>
-      </div>
-    </div>
+    <Routes>
+      <Route index path="/" element={<Home />} />
+      <Route
+        path="person"
+        element={
+          <div>
+            <Outlet />
+          </div>
+        }
+      >
+        <Route index element={<Navigate to="/" replace />} />
+        <Route path=":name" element={<div>person Name</div>} />
+      </Route>
+      <Route path="*" element={<div>404 - Page Not Found</div>} />
+    </Routes>
   );
 }
 
---
+++ src/home/Home.tsx
@@ -0,0 +1,42 @@
+import { ModeToggle } from "@/_components/ModeToggle";
+import { Language } from "@/_translations/i18n";
+import { Button } from "@/components/ui/button";
+import { useTranslation } from "react-i18next";
+
+function Home() {
+  const { i18n, t } = useTranslation();
+
+  const handleLanguageChange = (language: string) => {
+    i18n.changeLanguage(language);
+    localStorage.setItem("i18nextLng", language);
+  };
+
+  console.log("Current language:", i18n.language, t("HOME.TITLE"));
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="text-center">
+        <p className="text-3xl font-bold underline">WORK IN PROGRESS</p>
+        <p className="text-xl italic mt-4">
+          The verry beginning of the convention website of family Van Hese
+        </p>
+        <ModeToggle />
+        <Button
+          onClick={() => handleLanguageChange(Language.en)}
+          disabled={i18n.language === Language.en}
+        >
+          {"CHANGE LANG TO EN"}
+        </Button>
+        <Button
+          onClick={() => handleLanguageChange(Language.nl)}
+          disabled={i18n.language === Language.nl}
+        >
+          {"CHANGE LANG TO NL"}
+        </Button>
+        <p>{t("HOME.TITLE")}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
